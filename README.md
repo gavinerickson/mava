826fda3810523807eea581c365688369258caa80
Gavin Erickson <gavin@gavins-mbp.home>
2016-11-08 00:25:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+mava
+====
+
+A Symfony project created on November 3, 2016, 12:13 am.
---
+++ src/AppBundle/DataFixtures/ORM/LoadUsers.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gavin
+ * Date: 03/11/2016
+ * Time: 21:16
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+
+class LoadUsers implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $user1 = new User();
+        $user1->setName('John');
+        $user1->setBio('He is a cool guy');
+        $user1->setEmail('john@mava.info');
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setName('Jack');
+        $user2->setBio('He is a cool guy too');
+        $user2->setEmail('jack@mava.info');
+        $manager->persist($user2);
+
+        $manager->flush();
+    }
+}
